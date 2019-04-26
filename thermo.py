2bdcb207f059f628ecfadaca1799ec8803ede21c
wxmann <jim.z.tang@gmail.com>
2019-04-26 02:01:55 -0700

    calculate virtual temp correction

--- thermo.py
+++ thermo.py
@@ -1,21 +1,38 @@
 from collections import namedtuple
 
 import metpy.calc as mpcalc
+from metpy.units import units
+import numpy as np
+
+Quantity = units.Quantity
 
 Ob = namedtuple('Ob', ['p', 'T'])
 
 
 def sbparcel(p, T, Td):
     p0, T0, Td0 = p[0], T[0], Td[0]
-    return ParcelThermoCalculator(p, T, Td, p0, T0, Td0)
+    return Parcel(p, T, Td, p0, T0, Td0)
 
 
 def mlparcel(p, T, Td):
     pml, Tml, Tdml = mpcalc.mixed_parcel(p, T, Td)
-    return ParcelThermoCalculator(p, T, Td, pml, Tml, Tdml)
+    return Parcel(p, T, Td, pml, Tml, Tdml)
 
 
-class ParcelThermoCalculator(object):
+def virtualtemp(p, T, Td):
+    r = mpcalc.saturation_mixing_ratio(p, Td)
+    vt_raw = mpcalc.virtual_temperature(T, r).to('kelvin')
+    Tk = T.to('kelvin')
+    p_hpa = p.to('hPa')
+
+    # in a perfect world we're done here, but in many raobs the dewpoint data at higher altitudes
+    # is missing. Fortunately the virtual temperature correction is on order of ~0.2 K or less
+    # above 500mb. So we fill in the original temperature for missing VT's above that level.
+    vt_corrected = np.where((np.isnan(vt_raw.magnitude) & (p_hpa.magnitude < 500)), Tk, vt_raw)
+    return Quantity(vt_corrected, 'kelvin')
+
+
+class Parcel(object):
     def __init__(self,
                  environ_p, environ_T, environ_Td,
                  parcel_p0, parcel_T0, parcel_Td0):
@@ -27,13 +44,41 @@ class ParcelThermoCalculator(object):
         T2[0] = parcel_T0
         Td2[0] = parcel_Td0
 
-        self._parcelp, newT, newTd, self._parcelT = mpcalc.parcel_profile_with_lcl(p2, T2, Td2)
+        self._parcelp, Twithlcl, Tdwithlcl, self._parcelT = mpcalc.parcel_profile_with_lcl(p2, T2, Td2)
         self._lclp, self._lclT = mpcalc.lcl(parcel_p0, parcel_T0, parcel_Td0)
-        self._cape, self._cin = mpcalc.cape_cin(self._parcelp, newT, newTd, self._parcelT)
-        self._lfcp, self._lfcT = mpcalc.lfc(self._parcelp, newT, newTd, self._parcelT)
-        self._elp, self._elT = mpcalc.el(self._parcelp, newT, newTd, self._parcelT)
 
-    def parcel_profile(self, pres_unit='hPa', temp_unit='degC'):
+        # buoyancy quantities, no virtual temperature correction
+        self._cape, self._cin = mpcalc.cape_cin(self._parcelp, Twithlcl, Tdwithlcl, self._parcelT)
+        self._lfcp, self._lfcT = mpcalc.lfc(self._parcelp, Twithlcl, Tdwithlcl, self._parcelT)
+        self._elp, self._elT = mpcalc.el(self._parcelp, Twithlcl, Tdwithlcl, self._parcelT)
+
+        # virtual temperature correction
+        self._parcelp_vt, self._parcelT_vt = self._parcel_vt_correct()
+        Tvirt = virtualtemp(self._parcelp, Twithlcl, Tdwithlcl)
+
+        # TODO: apply LFC patch when LFC is unbounded
+        self._cape_vt, self._cin_vt = mpcalc.cape_cin(self._parcelp, Tvirt, Tdwithlcl, self._parcelT_vt)
+        self._lfcp_vt, self._lfcT_vt = mpcalc.lfc(self._parcelp, Tvirt, Tdwithlcl, self._parcelT_vt)
+        self._elp_vt, self._elT_vt = mpcalc.el(self._parcelp, Tvirt, Tdwithlcl, self._parcelT_vt)
+
+
+    def _parcel_vt_correct(self):
+        parcelp_Tv = self._parcelp
+
+        # Below the LCL, the parcel is lifted with a constant mixing ratio (that which reaches
+        # saturation at the LCL). Above the LCL, the parcel is saturated so the
+        # mixing ratio = the saturation mixing ratio at each parcel ascent point.
+        r_lcl = mpcalc.saturation_mixing_ratio(self._lclp, self._lclT)
+        corrected_below_lcl = mpcalc.virtual_temperature(self._parcelT, r_lcl).to('kelvin')
+        corrected_above_lcl = mpcalc.virtual_temperature(
+            self._parcelT,
+            mpcalc.saturation_mixing_ratio(self._parcelp, self._parcelT)).to('kelvin')
+
+        parcel_tv_corrected = np.where(parcelp_Tv >= self._lclp, corrected_below_lcl, corrected_above_lcl)
+        return parcelp_Tv, Quantity(parcel_tv_corrected, 'kelvin')
+
+
+    def profile(self, pres_unit='hPa', temp_unit='degC'):
         return Ob(p=self._parcelp.to(pres_unit), T=self._parcelT.to(temp_unit))
 
     def lcl(self, pres_unit='hPa', temp_unit='degC'):
@@ -50,3 +95,15 @@ class ParcelThermoCalculator(object):
 
     def cin(self):
         return self._cin
+
+    def lfc_vt(self, pres_unit='hPa', temp_unit='degC'):
+        return Ob(p=self._lfcp_vt.to(pres_unit), T=self._lfcT_vt.to(temp_unit))
+
+    def el_vt(self, pres_unit='hPa', temp_unit='degC'):
+        return Ob(p=self._elp_vt.to(pres_unit), T=self._elT_vt.to(temp_unit))
+
+    def cape_vt(self):
+        return self._cape_vt
+
+    def cin_vt(self):
+        return self._cin_vt
